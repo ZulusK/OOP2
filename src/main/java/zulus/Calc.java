a33c633d831eb3687478e6aa84801e8dd12ce0a9
Danil Kazimirov <danil.kazim.99@gmail.com>
2018-02-20 07:00:49 +0200

    add: + style for site + javadoc building

--- src/main/java/zulus/Calc.java
+++ src/main/java/zulus/Calc.java
@@ -1,6 +1,15 @@
 package zulus;
 
+/**
+ * Some class f
+ */
 public class Calc {
+    /**
+     * function a
+     * @param a
+     * @param b
+     * @return
+     */
     public static int sum(int a, int b) {
         return a + b;
     }
